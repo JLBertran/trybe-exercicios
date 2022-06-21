8eabf21bf26baa4ec1dec5aa2f94dfb681ebf1c0
jean <jeanbertran@outlook.com>
2022-06-21 20:13:35 -0300

    Atividade 05 feita

---
+++ bloco-4-js-primeiros-passos/dia-1-js-primeiros-passos/js-atv05-d1.js
@@ -0,0 +1,15 @@
+
+
+const a = 100;
+const b = 40;
+const c = 40;
+
+if (a + b + c === 180) {
+    resultado = true;
+    console.log(resultado);
+} else {
+    resultado = false;
+    console.log(resultado);
+}
+
+
