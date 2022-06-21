2ed4811f595b7e43e4ed5c292a7cf151e5064c88
jean <jeanbertran@outlook.com>
2022-06-21 15:57:23 -0300

    inicio bloco 4 js

---
+++ bloco-4-js-primeiros-passos/dia-1-js-primeiros-passos/js-atv-d1.js
@@ -0,0 +1,11 @@
+
+const myName = "Jean";
+const birthCity = 'São José dos Campos';
+var birthYear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+console.log(birthCity);
+console.log(birthCity);
+
