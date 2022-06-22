32c1745f9478edf6d01333234fb1253eaa6e4fc7
jean <jeanbertran@outlook.com>
2022-06-22 18:58:33 -0300

    Atividade Video 02 feita

--- bloco-4-js-primeiros-passos/dia-2-js-array-e-loop-for/js-v-atv02-d1.js
+++ bloco-4-js-primeiros-passos/dia-2-js-array-e-loop-for/js-v-atv02-d1.js
@@ -2,8 +2,8 @@
 
 let cars = ["Saab", "Volvo", "BMW"];
 
-for (let index = 0; index < cars.length; index += 1) {
-  console.log(cars[index]);
+for (let inde = 0; inde < cars.length; inde += 1) {
+  console.log(cars[inde]);
 }
 
 // Utilize o for para imprimir os elementos da lista groceryList com o console.log():
@@ -19,13 +19,12 @@ for (let i = 0; i < groceryList.length; i += 1) {
 // o of ele itera os numeros dentro do array e apresenta como resultado.
 // o of ele não altera os valores de dentro do array
 
-let numeros = [1,2,3,4,5];
-for(let numero of numeros) {
+let numeros = [1, 2, 3, 4, 5];
+for (let numero of numeros) {
   console.log(numero);
 }
 
-
-let word = 'Hello';
+let word = "Hello";
 for (let letter of word) {
   console.log(letter);
 }
@@ -38,12 +37,35 @@ for (let sum of arrOfNumbers) {
 
 //Utilize o for/of para imprimir os elementos da lista names com o console.log():
 
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+let names = ["João", "Maria", "Antônio", "Margarida"];
 
 for (let nameList of names) {
-    console.log(nameList);
+  console.log(nameList);
+}
 
+for (let index = 0; index < 10; index += 1) {
+  console.log(index);
+}
+
+for (let indes = 5; indes < 15; indes += 1) {
+  console.log("Oie");
 }
 
 
+let counter = 0;
 
+while(counter !== 5) {
+  counter += 1;
+  console.log(counter)
+}
+
+
+function dice () { 
+    return Math.ceil(Math.random() * 6);
+}
+let d1 = dice();
+let d2 = dice();
+while (d1 === d2) {
+    d2 = dice();
+    console.log(d2);
+}
