5845f749d47920daa6db102619b7a4a4c0a84792
jean <jeanbertran@outlook.com>
2022-06-21 21:29:27 -0300

    Atividade 08 feita

---
+++ bloco-4-js-primeiros-passos/dia-1-js-primeiros-passos/js-atv08-d1.js
@@ -0,0 +1,17 @@
+
+const A = 3;
+const B = 7;
+const C = 5;
+
+let even = true;
+let odd = false;
+
+
+if (A % 2 == 0 || B % 2 == 0 || C % 2 == 0) {
+    resultado = even;
+    console.log(resultado,"par")
+} else {
+    resultado = odd;
+    console.log(resultado,"impar")
+}
+
