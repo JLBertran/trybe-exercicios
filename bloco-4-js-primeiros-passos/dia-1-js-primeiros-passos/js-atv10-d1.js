50c5edb9d9d8eebaf26c2f0a46c7a96743ac6eef
jean <jeanbertran@outlook.com>
2022-06-21 21:41:53 -0300

    Atividade 10 feita

---
+++ bloco-4-js-primeiros-passos/dia-1-js-primeiros-passos/js-atv10-d1.js
@@ -0,0 +1,14 @@
+
+
+const valorVenda = 450;
+console.log("valor de venda", valorVenda)
+const valorCusto = 200;
+console.log("custo", valorCusto)
+let taxas = valorCusto * 0.20;
+console.log("taxas",taxas)
+let custoTotal = valorCusto + taxas;
+console.log("custototal", custoTotal)
+let lucro = valorVenda - custoTotal;
+console.log(lucro);
+
+
