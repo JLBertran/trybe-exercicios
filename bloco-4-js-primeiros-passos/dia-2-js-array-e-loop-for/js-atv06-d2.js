de31eaedb43b5989e6a1d0d77efab01da66cd601
jean <jeanbertran@outlook.com>
2022-06-23 00:56:18 -0300

    Atividade 06 feita

--- bloco-4-js-primeiros-passos/dia-2-js-array-e-loop-for/js-atv06-d2.js
+++ bloco-4-js-primeiros-passos/dia-2-js-array-e-loop-for/js-atv06-d2.js
@@ -2,12 +2,42 @@
 //Descubra quantos valores ímpares existem no array e imprima o resultado. 
 //Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let even = 0 +=1;
+// jeito que eu fiz para encontrar os numeros impares
 
-for (let index = 0; index <numbers.length; index +=1) {
-    if (even =   )
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let even = [];
+let odd = [];
+
+for (let index = 0; index < numbers.length; index +=1) {
+    numbers[index] % 2 == 0;
+    if (numbers[index] % 2 == 0 === true){
+        even.push(true);
+    } else if (numbers[index] % 2 == 0 === false) {
+        odd.push(false);
+    } else {
+        console.log("nenhum valor ímpar encontrado");
+
+    }
+
+    }
+    console.log(even.length, "numeros pares");
+    console.log(odd.length, "numeros impares");
+    
+
+// jeito certo de acordo com o gabarito da trybe 
+
+
+let number = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+for (let i = 0; i < number.length; i +=1 ){
+    if (number[i] % 2 !== 0) {
+        result +=1;
+    }
 }
 
-
-
+if (result === 0) {
+    console.log("nenhum valor ímpar encontrado");
+}else {
+    console.log(result);
+}
