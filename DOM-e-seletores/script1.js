bdf877c9e5346f279b8312bdaea4c66836b6002f
jean <jeanbertran@outlook.com>
2022-06-29 12:53:04 -0300

    atv 5.5 feita.

--- DOM-e-seletores/script1.js
+++ DOM-e-seletores/script1.js
@@ -1,18 +1,25 @@
-function exercicio1 () {
-let txtTagP = document.getElementsByTagName('p');
-txtTagP[1].innerText ='trabalhando como programador';
+function exercicio1() {
+  let txtTagP = document.getElementsByTagName("p");
+  txtTagP[1].innerText = "trabalhando como programador";
 }
-exercicio1()
+exercicio1();
 
-function exercicio2 () {
-let mainContent = document.querySelector('main');
-mainContent.style.backgroundColor= "rgb(76, 164, 109)";
+function exercicio2() {
+  let mainContent = document.querySelector("main");
+  mainContent.style.backgroundColor = "rgb(76, 164, 109)";
 }
-exercicio2 ()
+exercicio2();
 
-function exercicio3 () {
-let center = document.getElementsByClassName('center-content')[0];
-center.style.backgroundColor = 'white';
+function exercicio3() {
+  let center = document.getElementsByClassName("center-content")[0];
+  center.style.backgroundColor = "white";
 }
-exercicio3 ()
+exercicio3();
 
+function exercicio4() {
+  let title = document.querySelector("h1");
+  title.innerText = "Exercício 5.1 - JavaScript";
+}
+exercicio4();
+
+func
