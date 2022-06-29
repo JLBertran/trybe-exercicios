388e048ca03a3ef860fba21c083a11b2f457b485
jean <jeanbertran@outlook.com>
2022-06-29 12:35:22 -0300

    atv 5.3 feita.

--- DOM-e-seletores/script1.js
+++ DOM-e-seletores/script1.js
@@ -1,2 +1,8 @@
 const txtTagP = document.getElementsByTagName('p');
-txtTagP[1].innerText ='trabalhando como programador'
+txtTagP[1].innerText ='trabalhando como programador';
+
+const mainContent = document.querySelector('main');
+mainContent.style.backgroundColor = "rgb(76, 164, 109)";
+
+
+ 
