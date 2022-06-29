b11cf906c7283e0c37d6ac535b5f78c36d82e544
jean <jeanbertran@outlook.com>
2022-06-29 12:49:59 -0300

    reli os enunciados e adicionei funções

--- DOM-e-seletores/script1.js
+++ DOM-e-seletores/script1.js
@@ -1,8 +1,18 @@
-const txtTagP = document.getElementsByTagName('p');
+function exercicio1 () {
+let txtTagP = document.getElementsByTagName('p');
 txtTagP[1].innerText ='trabalhando como programador';
+}
+exercicio1()
 
-const mainContent = document.querySelector('main');
-mainContent.style.backgroundColor = "rgb(76, 164, 109)";
+function exercicio2 () {
+let mainContent = document.querySelector('main');
+mainContent.style.backgroundColor= "rgb(76, 164, 109)";
+}
+exercicio2 ()
 
+function exercicio3 () {
+let center = document.getElementsByClassName('center-content')[0];
+center.style.backgroundColor = 'white';
+}
+exercicio3 ()
 
- 
